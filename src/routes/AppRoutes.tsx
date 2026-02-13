8d47f08af46e104aca2ff7c4b4a01ff9070225ee
Prithivi <prithivi@users.noreply.github.com>
2026-02-13 15:39:31 +0545

    feat: add HomePage route under AppLayout for improved navigation

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import AppLayout from "@/layout/AppLayout";
+import HomePage from "@/pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}></Route>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   );
 };
