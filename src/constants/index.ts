7fe6739522d3591674f7b3a8345a2c156b0919dd
Prithivi <prithivi@users.noreply.github.com>
2026-02-13 13:16:17 +0545

    feat: add SIDEBAR_ITEMS constant for sidebar navigation

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+import {
+  CalendarIcon,
+  HomeIcon,
+  InboxIcon,
+  SearchIcon,
+  SettingsIcon,
+} from "lucide-react";
+
+export const SIDEBAR_ITEMS = [
+  {
+    title: "Home",
+    url: "/",
+    icon: HomeIcon,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: InboxIcon,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: CalendarIcon,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: SearchIcon,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: SettingsIcon,
+  },
+];
