a7f4d27c98d7b592727db8e8e056b54b2ff04e0c
Prithivi <prithivi@users.noreply.github.com>
2026-02-13 15:39:37 +0545

    feat: create HomePage component with area, bar, and pie charts

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+
+const HomePage = () => {
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Text</div>
+    </section>
+  );
+};
+
+export default HomePage;
