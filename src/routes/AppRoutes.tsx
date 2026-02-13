abd128d9e4f593150733644439002b7a905f55b7
Prithivi <prithivi@users.noreply.github.com>
2026-02-13 23:22:58 +0545

    feat: add UserPage route to AppRoutes for user management

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import AppLayout from "@/layout/AppLayout";
 import HomePage from "@/pages/HomePage";
+import UserPage from "@/pages/UserPage";
 import { Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
@@ -7,6 +8,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/users" element={<UserPage />} />
       </Route>
     </Routes>
   );
