0c46b908d7b76d203e81238234e50eb8725c8425
Prithivi <prithivi@users.noreply.github.com>
2026-02-14 12:52:24 +0545

    feat: update routing to include UsersPage and Payment components

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,18 @@
 import AppLayout from "@/layout/AppLayout";
 import HomePage from "@/pages/HomePage";
 import UserPage from "@/pages/UserPage";
+import UsersPage from "@/pages/UsersPage";
 import { Route, Routes } from "react-router-dom";
+import Payment from "@/components/Payment";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/users" element={<UserPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/:id" element={<UserPage />} />
+        <Route path="/payments" element={<Payment />} />
       </Route>
     </Routes>
   );
