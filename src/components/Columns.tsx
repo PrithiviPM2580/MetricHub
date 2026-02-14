18b514fca29265bf97b9ddb9db2eed24a1b65393
Prithivi <prithivi@users.noreply.github.com>
2026-02-14 14:24:58 +0545

    feat: enhance Columns component with dropdown actions and status styling

--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,28 +1,127 @@
+import { cn } from "@/lib/utils";
 import { type ColumnDef } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Checkbox } from "./ui/checkbox";
 
 export type PaymentType = {
   id: number;
   amount: number;
-  status: "completed" | "pending" | "failed";
+  status: "success" | "pending" | "failed";
   email: string;
   username: string;
 };
 
 export const columns: ColumnDef<PaymentType>[] = [
   {
+    id: "select",
+    header: ({ table }) => {
+      return (
+        <Checkbox
+          checked={
+            table.getIsAllRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+        />
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+        />
+      );
+    },
+  },
+  {
     accessorKey: "username",
     header: "User",
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status");
+
+      return (
+        <div
+          className={cn(
+            "px-2 py-1 rounded-xl w-max text-xs",
+            status === "success" && "bg-green-500",
+            status === "pending" && "bg-yellow-500",
+            status === "failed" && "bg-red-500",
+          )}
+        >
+          {status as string}
+        </div>
+      );
+    },
   },
   {
     accessorKey: "amount",
-    header: "Amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formattedAmount = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formattedAmount}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() =>
+                navigator.clipboard.writeText(payment.id.toString())
+              }
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
