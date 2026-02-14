24858acf05e2913fd665a2065e01eda591be8482
Prithivi <prithivi@users.noreply.github.com>
2026-02-14 12:52:28 +0545

    feat: create UsersPage component with table imports

---
+++ src/pages/UsersPage.tsx
@@ -0,0 +1,15 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const UsersPage = () => {
+  return <div>UsersPage</div>;
+};
+
+export default UsersPage;
